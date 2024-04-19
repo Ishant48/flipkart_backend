346f4cb4b6b2ce0997f6a570863c6edd9faf0f59
Ishant <p0010@cdot.in>
2024-04-19 10:23:04 +0530

    port added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/',router)
-const PORT = 8000
+const PORT = process.env.PORT | 8000;
 
 app.listen(PORT, ()=>{
     console.log(`Server is Running on ${PORT}`);
